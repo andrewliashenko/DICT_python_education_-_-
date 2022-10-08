2dde2cac84400d6b4a225650aafc18c282a5009c
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-08 16:54:21 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,5 @@
 print("Hello! My name is Pythonchik_bot")
 print("I was created in 2022")
+print("Please, remind me your name")
+name_user = input(">")
+print(f'What a great name you have, {name_user}')

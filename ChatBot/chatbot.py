69871b8ecea6cb001cd064bff9cd70c50de53b4a
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-08 20:16:09 +0300

    ChatBot 5-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -14,4 +14,13 @@ print("Now I will prove to you that I can count to any number you want.")
 a = int(input(">"))
 for i in range(a+1):
     print(str(i)+ "!")
-
+print("Let's test your programming knowledge.")
+print("what programming language do you learn?\n1.c++\n2.Java\n3.Python")
+while True:
+    k = int(input(">"))
+    if k==3:
+        print("Completed!")
+        break
+    else:
+        print("Please, try again.")
+print("Congratulations, have a nice day!")

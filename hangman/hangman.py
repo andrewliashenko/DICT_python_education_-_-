736dc3d9be27ffb77b5dcc075e66400931064e72
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-14 23:25:57 +0300

    Stage 2-nd hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,2 +1,10 @@
-print(str.upper("hangsman"))
-print(str.capitalize("the game will be available soon."))
+print(str.upper("hangman"))
+print("Welcome to the game.\nGuess the word to survive")
+my_list = ["python", "java", "javascript", "php"]
+k = str(input(">"))
+if k == my_list[0]:
+    print("You survived!")
+else:
+    print("You lost!")
+
+

7ce665653231dcebb0681da925dd06ef0ed63ebc
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-14 23:53:26 +0300

    Stage 3-rd hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,10 +1,9 @@
 print(str.upper("hangman"))
 print("Welcome to the game.\nGuess the word to survive")
+import random
 my_list = ["python", "java", "javascript", "php"]
 k = str(input(">"))
-if k == my_list[0]:
+if k == random.choice(my_list):
     print("You survived!")
 else:
     print("You lost!")
-
-

493ca9d363d4e5774e13a7fcfc9b66a4afbc8c7e
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-15 12:37:30 +0300

    Stage 4-th hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,9 +1,10 @@
 print(str.upper("hangman"))
 print("Welcome to the game.\nGuess the word to survive")
+my_list = ["python", "java", "javascript", "kotlin"]
 import random
-my_list = ["python", "java", "javascript", "php"]
-k = str(input(">"))
-if k == random.choice(my_list):
+randon_word = (random.choice(my_list))
+k = input("This word is: " + randon_word[:3] + "-".join(['' for _ in range(len(randon_word)-2)]) + ">")
+if k == randon_word:
     print("You survived!")
 else:
     print("You lost!")

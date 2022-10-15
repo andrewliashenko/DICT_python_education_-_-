47fed569433347916e3c261f2218951115c023fa
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-15 14:39:17 +0300

    Stage 5-th hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,10 +1,23 @@
-print(str.upper("hangman"))
-print("Welcome to the game.\nGuess the word to survive")
-my_list = ["python", "java", "javascript", "kotlin"]
+print("HANGMAN")
+print("The game will be available soon.")
+my_list = ["python", "java", "kotlin", "javascript"]
 import random
-randon_word = (random.choice(my_list))
-k = input("This word is: " + randon_word[:3] + "-".join(['' for _ in range(len(randon_word)-2)]) + ">")
-if k == randon_word:
-    print("You survived!")
-else:
-    print("You lost!")
+randon_word = random.choice(my_list)
+remember_letters = []
+all_letters = list(set(randon_word))
+words = ''.join([i if i in remember_letters else "-" for i in randon_word])
+print(words)
+life = 8
+while life > 0:
+    letter = input("input a letter:")
+    if letter in randon_word and letter not in remember_letters:
+        remember_letters.append(letter)
+        all_letters.remove(letter)
+    else:
+        life -= 1
+        print("That letter doesn't appear in the word")
+    words = ''.join([i if i in remember_letters else "-" for i in randon_word])
+    print(words)
+    if len(all_letters) == 0:
+        break
+print("thanks for playing\nWe'll see how well you did in the next stage")

623eb4284c9560c30772affe27c366df21aa8ab9
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-19 20:27:49 +0300

    Stage 4-th

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -7,13 +7,20 @@ if number_of_people > 0:
     for i in range(int(number_of_people)):
         names = input(">")
         payments[names] = 0
-    total = int(input("Enter the total amount:\n>"))
-    a = round(total / number_of_people, 2)
+    total_price = int(input("Enter the total amount:\n>"))
+    a = round(total_price / number_of_people, 2)
     print('Do you want to use the "Who is lucky?" feature? Write yes/no:')
     lucky = str(input(">"))
     lucky_name = random.choice(list(payments.keys()))
     if lucky == "yes":
         print(lucky_name, "is the lucky one!")
+        payments[lucky_name] = 0
+        lucky_price = round(total_price / (number_of_people - 1))
+        for p in payments.keys():
+            payments[p] = lucky_price
+        if lucky:
+            payments[lucky_name] = 0
+        print(payments)
     elif lucky == "no":
         print("No one is going to be lucky")
         for names in payments:

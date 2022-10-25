2e6dc85bc444ad2934c716ca0636cc90d751b43c
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-25 13:19:08 +0300

    Stage 1-st

---
+++ tictactoe/tictactoe.py
@@ -0,0 +1,7 @@
+area = ["1.1", "1.2", "1.3", "2.1", "2.2", "2.3", "3.1", "3.2", "3.3"]
+a = f"| {area[0]} | {area[1]} | {area[2]} |" \
+    f"\n| {area[3]} | {area[4]} | {area[5]} |" \
+    f"\n| {area[6]} | {area[7]} | {area[8]} |"
+print(a)
+x = str(input("Enter coordinates X \n>"))
+

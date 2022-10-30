8d34b158b2e47bb07fd8c1beb6fe8be624dd3602
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-30 22:25:01 +0200

    2-nd stage

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,5 +3,19 @@ a = f"| {area[0]} | {area[1]} | {area[2]} |" \
     f"\n| {area[3]} | {area[4]} | {area[5]} |" \
     f"\n| {area[6]} | {area[7]} | {area[8]} |"
 print(a)
-x = str(input("Enter coordinates X \n>"))
+life = 0
+while True:
 
+    coordinates_x = area.index(str(input("Enter coordinates X\n>")))
+    area[coordinates_x] = " X "
+    a = f"| {area[0]} | {area[1]} | {area[2]} |" \
+        f"\n| {area[3]} | {area[4]} | {area[5]} |" \
+        f"\n| {area[6]} | {area[7]} | {area[8]} |"
+    print(a)
+
+    coordinates_o = area.index(str(input("Enter coordinates 0\n>")))
+    area[coordinates_o] = " 0 "
+    a = f"| {area[0]} | {area[1]} | {area[2]} |" \
+        f"\n| {area[3]} | {area[4]} | {area[5]} |" \
+        f"\n| {area[6]} | {area[7]} | {area[8]} |"
+    print(a)

7be4b3c207351f44ce54298f669ea2a5a0603a17
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-31 09:44:02 +0200

    4-th  stage

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -15,15 +15,23 @@ while True:
     n += 1
     c = dict_area[" X "] = n
     if dict_area[" X "] == 5:
-        print("end")
+        print("DEAD HEAT")
         break
-
+    wins = [(area[0:3]), (area[3:6]), (area[6:9]), (area[0], area[3], area[6]),
+            (area[1], area[4], area[7]), (area[2], area[5], area[8]),
+            (area[0], area[4], area[8]), (area[2], area[4], area[6])]
+    if (" X ", " X ", " X ") in wins or [" X ", " X ", " X "] in wins:
+        print("X win")
+        break
     coordinates_o = area.index(str(input("Enter coordinates 0\n>")))
     area[coordinates_o] = " 0 "
     a = f"| {area[0]} | {area[1]} | {area[2]} |" \
         f"\n| {area[3]} | {area[4]} | {area[5]} |" \
         f"\n| {area[6]} | {area[7]} | {area[8]} |"
     print(a)
-
-
-
+    wins = [(area[0:3]), (area[3:6]), (area[6:9]), (area[0], area[3], area[6]),
+            (area[1], area[4], area[7]), (area[2], area[5], area[8]),
+            (area[0], area[4], area[8]), (area[2], area[4], area[6])]
+    if (" 0 ", " 0 ", " 0 ") in wins or [" 0 ", " 0 ", " 0 "] in wins:
+        print("0 win")
+        break

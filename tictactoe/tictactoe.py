d4768c17c7974b5c3e3b31ee4889a30015542400
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-10-31 08:55:06 +0200

    3-rd  stage

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,15 +3,20 @@ a = f"| {area[0]} | {area[1]} | {area[2]} |" \
     f"\n| {area[3]} | {area[4]} | {area[5]} |" \
     f"\n| {area[6]} | {area[7]} | {area[8]} |"
 print(a)
-life = 0
+n = 0
 while True:
-
     coordinates_x = area.index(str(input("Enter coordinates X\n>")))
     area[coordinates_x] = " X "
     a = f"| {area[0]} | {area[1]} | {area[2]} |" \
         f"\n| {area[3]} | {area[4]} | {area[5]} |" \
         f"\n| {area[6]} | {area[7]} | {area[8]} |"
     print(a)
+    dict_area = dict.fromkeys(area)
+    n += 1
+    c = dict_area[" X "] = n
+    if dict_area[" X "] == 5:
+        print("end")
+        break
 
     coordinates_o = area.index(str(input("Enter coordinates 0\n>")))
     area[coordinates_o] = " 0 "
@@ -19,3 +24,6 @@ while True:
         f"\n| {area[3]} | {area[4]} | {area[5]} |" \
         f"\n| {area[6]} | {area[7]} | {area[8]} |"
     print(a)
+
+
+

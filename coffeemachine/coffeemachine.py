079ff3f85aa63171b14f410b24df509012254112
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-11-05 23:22:57 +0200

    3-rd stage

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -1,22 +1,43 @@
-water = int(input("Write how many ml of water the coffee machine has:\n>"))
-milk = int(input("Write how many ml of milk the coffee machine has:\n>"))
-coffee = int(input("Write how many grams of coffee beans the coffee machine has:\n>"))
-number_of_cups = int(input("Write how many cups of coffee you will need:\n>"))
+water = 400
+milk = 540
+coffee = 120
+number_of_cups = 9
+money = 550
 
 
-def recipe():
-    a = 200 * number_of_cups
-    b = 50 * number_of_cups
-    c = 15 * number_of_cups
+def recipe(a, b, c, cup, m):
     k = [water//200, milk//50, coffee//15]
     n = min(k) - number_of_cups
-    if water >= a and milk >= b and coffee >= c and n == 1:
-        print("Yes, I can make that amount of coffee")
-    elif water >= a and milk >= b and coffee >= c and n >= 2:
-        print(f"Yes, I can make that amount of coffee and even {n} more than that")
-    elif water <= a or milk <= b or coffee <= c:
-        print(f"No, I can make only {-n} cups of coffee")
+    print(f"The coffee machine has:"
+          f"\n{water - a} of water"
+          f"\n{milk - b} of milk"
+          f"\n{coffee - c} of coffee beans"
+          f"\n{number_of_cups - cup} of disposable cups"
+          f"\n{money + m} of money")
 
 
-recipe()
+print(f"The coffee machine has:"
+      f"\n{water} of water"
+      f"\n{milk} of milk"
+      f"\n{coffee} of coffee beans"
+      f"\n{number_of_cups} of disposable cups"
+      f"\n{money} of money")
 
+action = input("Write action (buy, fill, take):\n>")
+if action == "buy":
+    buy = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n>"))
+    if buy == 1:
+        recipe(250, 0, 16, 1, 4)
+    elif buy == 2:
+        recipe(350, 75, 20, 1, 7)
+    elif buy == 3:
+        recipe(200, 100, 12, 1, 6)
+elif action == "fill":
+    a = int(input("Write how many ml of water you want to add:\n>"))
+    b = int(input("Write how many ml of milk you want to add:\n>"))
+    c = int(input("Write how many grams of coffee beans you want to add:\n>"))
+    cup = int(input("Write how many disposable coffee cups you want to add:\n>"))
+    recipe(-a, -b, -c, -cup, 0)
+elif action == "take":
+    print(f"I gave you {money}")
+    recipe(0, 0, 0, 0, -money)

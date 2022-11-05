ffa034d662f8ea3478fece7238ce5f63ce359553
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-11-05 21:16:57 +0200

    2-nd stage

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -1,14 +1,21 @@
+water = int(input("Write how many ml of water the coffee machine has:\n>"))
+milk = int(input("Write how many ml of milk the coffee machine has:\n>"))
+coffee = int(input("Write how many grams of coffee beans the coffee machine has:\n>"))
 number_of_cups = int(input("Write how many cups of coffee you will need:\n>"))
 
 
 def recipe():
-    a = 200
-    b = 50
-    c = 15
-    print(f"For {number_of_cups} cups of coffee you will need:"
-          f"\n{a*number_of_cups}ml of water"
-          f"\n{b*number_of_cups}ml of milk"
-          f"\n{c*number_of_cups}g of coffee beans")
+    a = 200 * number_of_cups
+    b = 50 * number_of_cups
+    c = 15 * number_of_cups
+    k = [water//200, milk//50, coffee//15]
+    n = min(k) - number_of_cups
+    if water >= a and milk >= b and coffee >= c and n == 1:
+        print("Yes, I can make that amount of coffee")
+    elif water >= a and milk >= b and coffee >= c and n >= 2:
+        print(f"Yes, I can make that amount of coffee and even {n} more than that")
+    elif water <= a or milk <= b or coffee <= c:
+        print(f"No, I can make only {-n} cups of coffee")
 
 
 recipe()

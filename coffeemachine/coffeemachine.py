5db4c6aaf5a64a3af7cea89c7b8e2f3592a945ee
andrewliashenko <a.v.liashenko@student.khai.edu>
2022-11-06 10:37:46 +0200

    edits

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -11,9 +11,9 @@ def recipe(a, b, c, cup, m):
 
 def remain():
     print(f"The coffee machine has:"
-          f"\n{ingredients['water']} of water"
-          f"\n{ingredients['milk']} of milk"
-          f"\n{ingredients['coffee']} of coffee beans"
+          f"\n{ingredients['water']}ml of water"
+          f"\n{ingredients['milk']}ml of milk"
+          f"\n{ingredients['coffee']}g of coffee beans"
           f"\n{ingredients['number_of_cups']} of disposable cups"
           f"\n{ingredients['money']} of money")
 
@@ -37,24 +37,22 @@ while True:
             ingredient(250, 1, 16)
             recipe(a=250, b=0, c=16, cup=1, m=-4)
         elif buy == 2:
+            ingredient(350, 75, 20)
             recipe(350, 75, 20, 1, -7)
-            print("I have enough resources, making you a coffee!")
         elif buy == 3:
+            ingredient(200, 100, 12)
             recipe(200, 100, 12, 1, -6)
-            print("I have enough resources, making you a coffee!")
     elif action == "fill":
         a = int(input("Write how many ml of water you want to add:\n>"))
         b = int(input("Write how many ml of milk you want to add:\n>"))
         c = int(input("Write how many grams of coffee beans you want to add:\n>"))
         cup = int(input("Write how many disposable coffee cups you want to add:\n>"))
         recipe(-a, -b, -c, -cup, 0)
-        remain()
     elif action == "take":
         print(f"I gave you {ingredients['money']}")
         recipe(0, 0, 0, 0, ingredients['money'])
         remain()
     elif action == "remaining":
-        recipe(0, 0, 0, 0, 0)
         remain()
     elif action == "exit":
         break

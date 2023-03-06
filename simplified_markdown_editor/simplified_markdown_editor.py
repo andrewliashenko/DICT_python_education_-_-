163bfa4f64ebc5ef19db56a55b4c947e3d6229d5
andrewliashenko <a.v.liashenko@student.khai.edu>
2023-03-06 10:57:10 +0200

    2st stage

--- simplified_markdown_editor/simplified_markdown_editor.py
+++ simplified_markdown_editor/simplified_markdown_editor.py
@@ -1,23 +1,15 @@
 def navigation():
     print("Available formatters: plain bold italic header\n" \
-           "link ordered-list unordered-list new-line \n" \
+           "link ordered-list unordered-list new-line space \n" \
            "Special commands: !help !done")
 
 
 def output(a):
     text.append(a)
-    print(" ".join(text))
+    print("".join(text))
 
 
-plain = ""
-bold = "**"
-italic = "*"
-header = {"H1": "#", "H2": "##", "H3": "###", "H4": "####"}
-link = ["[", "]", "(", ")"]
-ordered_list = "n"
-unordered_list = "*"
-new_line = "\n"
-formatter = ["plain", "bold", "italic", "header", "link", "ordered-list", "new-line", "!help", "!done"]
+formatter = ["space", "plain", "bold", "italic", "header", "link", "ordered-list", "unordered-list", "new-line", "!help", "!done"]
 text = []
 
 while True:
@@ -28,12 +20,53 @@ while True:
             output(plain_txt)
         elif enter == "bold":
             bold_txt = input("Text:>")
-            result = "".join([bold, bold_txt, bold])
+            result = "".join(["**", bold_txt, "**"])
             output(result)
-
         elif enter == "italic":
-            print("italic")
-        else:
-            print("!")
+            italic_txt = input("Text:>")
+            result = "".join(["*", italic_txt, "*"])
+            output(result)
+        elif enter == "header":
+            level = int(input("Level:>"))
+            header_txt = input("Text:>")
+            header = ""
+
+            for i in range(level):
+                header += "#"
+            result = "".join([header, header_txt])
+            output(result)
+        elif enter == "link":
+            label = input("Label:>")
+            link = input("Link:>")
+            result = "".join(["[", label, "]","(", link, ")"])
+            output(result)
+        elif enter == "ordered-list":
+            n = 0
+            number = int(input("Number of rows: >"))
+            or_list = []
+            for i in range(number):
+                n += 1
+                element = input(f"Row #{n}:>")
+                result = f"{n}.{element}\n"
+                text.append(result)
+            print("".join(text))
+        elif enter == "unordered-list":
+            number = int(input("Number of rows:>"))
+            or_list = []
+            n = 0
+            for i in range(number):
+                n += 1
+                element = input(f"Row #{n}:>")
+                result = f"*{element}\n"
+                text.append(result)
+            print("".join(text))
+        elif enter == "new-line":
+            text.append("\n")
+        elif enter == "space":
+            text.append(" ")
+        elif enter == "!help":
+            navigation()
+        elif enter == "!done":
+            break
     else:
         print("Unknown formatting type or command")

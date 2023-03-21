cb2044cab3d468aae7b7e61dc10924561e859ff3
andrewliashenko <a.v.liashenko@student.khai.edu>
2023-03-21 21:36:45 +0200

    2nd stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,15 +1,25 @@
 import random
-a = random.choice(range(2, 9))
-b = random.choice(range(2, 9))
-action = random.choice([a+b, a-b, a*b])
-if action == a+b:
-    print(f"{a}+{b}=")
-elif action == a-b:
-    print(f"{a}-{b}=")
-elif action == a*b:
-    print(f"{a}*{b}=")
-answer = int(input(">"))
-if answer == action:
-    print("Right!")
-else:
-    print("Wrong!")
+
+
+def mathem(n, k):
+    mark = 0
+    for _ in range(5):
+        a = random.choice(range(n, k))
+        b = random.choice(range(n, k))
+        action = random.choice([a+b, a-b, a*b])
+        if action == a+b:
+            print(f"{a}+{b}=")
+        elif action == a-b:
+            print(f"{a}-{b}=")
+        elif action == a*b:
+            print(f"{a}*{b}=")
+        answer = int(input(">"))
+        if answer == action:
+            print("Right!")
+            mark += 1
+        else:
+            print("Wrong!")
+    print(f"Your mark is {mark}/5.")
+
+
+mathem(2, 9)

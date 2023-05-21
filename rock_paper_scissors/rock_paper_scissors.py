a473f148d9d46d417425cc05fdeb73db7e6bb831
andrewliashenko <a.v.liashenko@student.khai.edu>
2023-05-21 15:18:17 +0300

    3st stage

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -5,27 +5,38 @@ def result():
     win = f"Well done. The computer chose {option_computer} and failed"
     lose = f"Sorry, but the computer chose {option_computer}"
     draw = f"There is a draw ({option_computer})"
-    n = 0
+    n = -1
     for i in range(3):
         n += 1
-        if n == 3:
-            n = 0
+        if n == -2:
+            n = 2
+        elif n == 2:
+            n = -1
+        elif n == 1:
+            n = -2
         if option_player == option[n]:
             if option_computer == option[n]:
                 print(draw)
+                rating[name] += 50
             elif option_computer == option[n+1]:
                 print(lose)
             elif option_computer == option[n+2]:
                 print(win)
-    if option_player not in option and option_player != "!exit":
+                rating[name] += 100
+    if option_player not in option and option_player != "!exit" and option_player != "!rating":
         print("Invalid input!")
 
 
+name = input("Enter your name: >")
+print(f"Hello, {name}")
+rating = {name: 0}
 while True:
     option_player = input(">")
     option = ["rock", "paper", "scissors"]
     option_computer = random.choice(option)
     result()
+    if option_player == "!rating":
+        print(f"{name}: {rating.values()}")
     if option_player == "!exit":
         print("Bue!")
         break
